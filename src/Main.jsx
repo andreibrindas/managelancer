4ecd8bcaf4310a8b02966091b5d787d64a4d9f8a
andreibrindas <andreibrindas@users.noreply.github.com>
2021-10-15 11:26:41 +0300

    long time no see

---
+++ src/Main.jsx
@@ -0,0 +1,20 @@
+function Main() {
+  return (
+    <div className="Main">
+      <h2>Clients</h2>
+
+      <div className="clientsTable">
+        <div className="clientsTable__header">
+          <div className="clientsTable__column">Nr.</div>
+          <div className="clientsTable__column">First Name</div>
+          <div className="clientsTable__column">Last Name</div>
+          <div className="clientsTable__column">Company</div>
+          <div className="clientsTable__column">Email</div>
+          <div className="clientsTable__column">Phone</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
